9160b5be5cc2580237914ee421848700a4ba20f8
Antipina <0671854750v@gmail.com>
2022-06-12 21:38:01 +0300

    2 and 3 tasks

---
+++ Teacher.java
@@ -0,0 +1,18 @@
+public class Teacher {
+
+    public static void main (String [] args) {
+        String subject = "Учитель Математики";
+        String firstName = "Иван Петрович";
+        String lastName = "Серебряков";
+        int amount = 55;
+        String line = "______________________________________________";
+
+        System.out.println(line);
+        System.out.println(subject);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(amount + " лет");
+        System.out.println(line);
+
+    }
+}
